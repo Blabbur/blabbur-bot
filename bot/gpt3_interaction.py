2d8464ed1c65960214a7bfdbe2aa1afee8d9e2e1
Faiz Surani <faiz.surani@gmail.com>
2021-01-16 17:10:56 -0800

    Update regex of random tweet

--- bot/gpt3_interaction.py
+++ bot/gpt3_interaction.py
@@ -2,11 +2,12 @@ import openai
 from dotenv import load_dotenv
 import os
 import random
-import regex
+import re
 
 
 def react_to_news():
-    react_instruction = "React negatively to the following piece of news: President-elect Joe Biden introduced his slate of scientific advisers with the promise that they would summon “science and " \
+    reaction_types = ["positively", "negatively", "angrily", "joyously"]
+    react_instruction = f'React to the following piece of news: "President-elect Joe Biden introduced his slate of scientific advisers with the promise that they would summon “science and ' \
                         "truth” to combat the pandemic, climate crisis and other challenges.\n"
     load_dotenv()
     openai.api_key = os.getenv('OPENAI_SECRET')
@@ -19,8 +20,8 @@ def reply_to_thread():
 
 
 def random_new_tweet():
-    tweet_types = ["hot-takes.txt", "jokes.txt"]
-    selected_type_file = f'prompts/{random.choice(tweet_types)}'
+    tweet_types = ["hot-takes", "jokes"]
+    selected_type_file = f'prompts/{random.choice(tweet_types)}.txt'
     tweets_of_category = open(selected_type_file).read().splitlines()
 
     # topics = ["Trump", "Facebook", "personality tests", "LeBron James", "smoking", "cooking", "astrology"]
@@ -34,10 +35,10 @@ def random_new_tweet():
     with open(selected_type_file, 'a') as fd:
         fd.write(f'\n{response}')
     category_filter_regex = "^.+?:(.*)"
-    regex_result = regex.match(category_filter_regex, response)
-    if regex_result and len(regex_result) > 1:
-        response = regex_result[1].strip()
+    regex_result = re.findall(category_filter_regex, response)
+    if regex_result and len(regex_result) >= 1:
+        response = regex_result[0].strip()
     return response
 
 
-print(react_to_news())
+print(random_new_tweet())
